727a304a8b38948edf2846385a5e7dd8089e1e39
x10an14 <chrischa@stud.ntnu.no>
2014-02-04 16:17:11 +0100

    Did part 1 of exercise 4. Have a bug though, my system's gcc can't find pow().

---
+++ exercises/ex4/ex4.c
@@ -0,0 +1,63 @@
+#include "../../examples/common/common.h"
+#include "ex4.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void fillVectorNumerically(Vector inpt){
+	for (int i = 0; i < inpt->glob_len; ++i){
+		inpt->data[i] = 1.0/(i+1);
+	}
+}
+
+double getVectorSum(Vector inpt){
+	double sum = 0.0;
+
+	for (int i = 0; i < inpt->glob_len; ++i){
+		sum += inpt->data[i];
+	}
+
+	return sum;
+}
+
+int main(int argc, char const *argv[]){
+	int vecLength = 0;
+
+	if(argc <= 1){
+		printf("Too few arguments given!\nProgram aborted.\n");
+		return -1;
+	} else{
+		vecLength = atoi(argv[1]);
+	}
+
+	//Generate vector "v"
+	Vector numericV = createVector(vecLength);
+	fillVectorNumerically(numericV);
+
+	//Compute sum of "v" on one processor.
+	//double vSum = getVectorSum(numericV);
+
+	//Set up vectors and "help-vectors" for computing the difference with different k-values
+	Vector kValues = createVector(12);
+	Vector difference = createVector(12);
+	Vector *vectorList = (Vector*) malloc(12*sizeof(Vector));
+	for (int i = 0; i < kValues->glob_len; ++i){
+		kValues->data[i] = 3+i;
+		vectorList[i] = createVector(3+i);
+		fillVectorNumerically(vectorList[i]);
+	}
+
+	//Compute the differences
+	for (int i = 0; i < kValues->glob_len; ++i){
+		difference->data[i] = (pow(PI, 2.0)/6.0) - getVectorSum(vectorList[i]);
+	}
+
+	//Print the differences
+	printf("\nBelow are the differences of the sums of vectors with the values: v[i] = 1/i^2,\nwhere n = 2^k, and k has been given different values.");
+	for (int i = 0; i < kValues->glob_len; ++i){
+		printf("With k = %d, the difference is: %.2f\n",
+			i+1, difference->data[i]);
+	}
+
+	return 0;
+}
---
+++ exercises/ex4/ex4.h
@@ -0,0 +1,10 @@
+#ifndef EXERCISE_4_TMA4280
+#define EXERCISE_4_TMA4280
+
+//Got value from wolfram alpha
+double PI = (double) 3.1415926535897932384626433832795028841971693993751058;
+
+void fillVectorNumerically(Vector inpt);
+double getVectorSum(Vector inpt);
+
+#endif
