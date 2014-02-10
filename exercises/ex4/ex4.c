861c08ddde6cc80790cd874aa4c81659a55a7858
x10an14 <x10an14@hotmail.com>
2014-02-10 17:49:32 +0100

    Started converting to use the MPI calls in common.h and common.c

--- exercises/ex4/ex4.c
+++ exercises/ex4/ex4.c
@@ -6,20 +6,6 @@
 #include "../../examples/common/common.h"
 #include "ex4.h"
 
-int rank,					// Rank of this process
-	size,					// Total number of processes
-	vecLength;				// Vector vecLength
-
-//MPI datatype
-MPI_Datatype vector;
-
-//Function for creating and committing MPI datatypes
-void create_types(){
-	//Creating vector type
-	MPI_Type_vector(vecLength, 1, vecLength, MPI_FLOAT, &vector);
-	MPI_Type_commit(&vector);
-}
-
 void fillVectorNumerically(Vector inpt){
 	for (int i = 0; i < inpt->len; ++i){
 		inpt->data[i] = 1.0/pow((i+1), 2.0);
@@ -40,51 +26,47 @@ double getVectorSum(Vector inpt){
 // TODO: TOP PRIORITY! Use common.h convenience functions for MPI
 // TODO: Add at argv[2] for number of MPI ranks
 int main(int argc, char *argv[]){
-	vecLength = 0;
+	int vecLength = 0, rank = 0, size = 0;
+	double wTime = 0.0;
 
-	if(argc <= 1){
+	#ifdef HAVE_MPI
+		//We can safely call it at this point in time because only rank 0 should be initialized afaik?
+		init_app(argc, argv, &rank, &size);
+		wTime = WallTime();
+	#endif
+
+	if(rank == 0 && argc <= 3){
 		printf("Too few arguments given!\n\tProgram aborted.\n");
 		return -1;
 	} else{
 		vecLength = atoi(argv[1]);
+		rank = atoi(argv[2]);
+		size = atoi(argv[3]);
 	}
 
-	// TODO: IMPORTANT! Make all MPI code dependent on presence of MPI. E.g. make IFDEFs
-	// Initialize MPI, get rank and size
-	MPI_Init(&argc, &argv); // argc: number of args, argv: arg-vector // TODO: Change?
-
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    // Create data-type(s)
-    create_types();
-
 	// Let rank 0 generate vector "v"
-	if (rank == 0)
-	{
+	#ifdef HAVE_MPI
+	if (rank == 0){
 		Vector numericV = createVector(vecLength);
 		fillVectorNumerically(numericV);
 	}
+	#endif
 
 	// TODO: Complete function call, Scatter data to MPI ranks
-	/* void *sendbuf, int sendcnt, MPI_Datatype sendtype, 
-               void *recvbuf, int recvcnt, MPI_Datatype recvtype, int root, 
-               MPI_Comm com */
-	int scatter_res = MPI_Scatter(void *sendbuf, int sendcnt, vector,
-               void *recvbuf, int recvcnt, vector, 0, 
-               MPI_COMM_WORLD);
-	
+	/* void *sendbuf, int sendcnt, MPI_Datatype sendtype,
+			   void *recvbuf, int recvcnt, MPI_Datatype recvtype, int root,
+			   MPI_Comm com */
+	//int scatter_res = MPI_Scatter(void *sendbuf, int sendcnt, vector, void *recvbuf, int recvcnt, vector, 0, MPI_COMM_WORLD);
+
 	// TODO: Convert to summing on local vector-piece if MPI is in use.
 	//Compute sum of "v" on processor(s).
 	//double vSum = getVectorSum(numericV);
-	
+
 	// TODO: Complete function call, Gather sums to rank 0 (preferably by binary tree for efficiency)
-	/* void *sendbuf, int sendcnt, MPI_Datatype sendtype, 
-               void *recvbuf, int recvcnt, MPI_Datatype recvtype, 
-               int root, MPI_Comm comm */
-	int gather_res = MPI_Gather(void *sendbuf, int sendcnt, vector, 
-               void *recvbuf, int recvcnt, vector, 
-               0, MPI_COMM_WORLD);
+	/* void *sendbuf, int sendcnt, MPI_Datatype sendtype,
+			   void *recvbuf, int recvcnt, MPI_Datatype recvtype,
+			   int root, MPI_Comm comm */
+	//int gather_res = MPI_Gather(void *sendbuf, int sendcnt, vector, void *recvbuf, int recvcnt, vector, 0, MPI_COMM_WORLD);
 
 	//Set up vectors and "help-vectors" for computing the difference with different k-values
 	Vector kValues = createVector(12);
@@ -109,8 +91,7 @@ int main(int argc, char *argv[]){
 	}
 
 	//MPI cleanup
-	MPI_Type_free(&vector);
-	MPI_Finalize();
+	close_app();
 
 	return 0;
 }
