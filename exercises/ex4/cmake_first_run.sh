60748d254e263fe3ec63f01f4bf657a71299afa9
x10an14 <x10an14@hotmail.com>
2014-02-11 19:27:33 +0100

    Added a comment and TODO.

--- exercises/ex4/cmake_first_run.sh
+++ exercises/ex4/cmake_first_run.sh
@@ -1,9 +1,12 @@
 #Create CMAKE sub-folders
 mkdir debug release -p
 
+#Flag to turn off OpenMP, TODO: Make bash ask if you want to use OpenMP
+#  -DENABLE_OPENMP=0
+
 #Create makefiles for each sub-folder
 cd debug/
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Debug 
 
 cd ../release/
 cmake .. -DCMAKE_BUILD_TYPE=Release
