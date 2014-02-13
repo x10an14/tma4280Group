181948839235b91a9511c8cea420000cc5a83a57
x10an14 <x10an14@hotmail.com>
2014-02-13 08:46:25 +0100

    KONGULL WORKSgit add ex4_jobscript.sh git add ex4_jobscript.sh ! =DDD

--- exercises/ex4/ex4_jobscript.sh
+++ exercises/ex4/ex4_jobscript.sh
@@ -11,28 +11,28 @@
 
 # Name of the job
 ###PBS -N myjob
-#PBS -N ex4_debug_no_OpenMP
+#PBS -N ex4_debug_no_MPI
 
 # Account to run job (change the account name: See https://www.hpc.ntnu.no/display/hpc/Kongull+Batch+Scheduling)
-###PBS -A freecycle
+#PBS -A freecycle
 
 # max walltime (must not exceed que max walltime)
 ###PBS -l walltime=00:04:00
 #PBS -l walltime=00:08:00
 
 # Specify resources number of nodes:cores per node
-###PBS -l nodes=1:ppn=1
-#PBS -l nodes=4:ppn=12:default
+#PBS -l nodes=1:ppn=1
+###PBS -l nodes=4:ppn=12:default
 
 # Specify queue to submit to: default, bigmem, express or default
-###PBS -q express
+###PBS -q express !!!! We are not permitted to run in this queue for this subject. !!!!
 #PBS -q optimist
 
 # Specify an email address to be notified on begin, abort, and end
 #PBS -M chrischa@stud.ntnu.no
 
 # Make errors go into the .o file, instead of splitting output to .o and .e
-###PBS -j oe
+#PBS -j oe
 
 ###### !!!!!make sure this is correct before you run this script!!!! ######
 # Cd to work directory
@@ -40,7 +40,7 @@ cd ${PBS_O_WORKDIR}
 
 module load intelcomp/13.0.1
 module load openmpi/1.4.3-intel #Should we load this module when we just test compile too?
-KMP_AFFINITY="granularity=fine, compact"
+#KMP_AFFINITY="granularity=fine, compact"
 
 #For-loop example in bash
 #for i in $(seq x y);
@@ -51,5 +51,5 @@ KMP_AFFINITY="granularity=fine, compact"
 for i in $(seq 3 14);
 	do
 		echo $i #There will be an empty newline between these two commands (AKA "\n\n")
-		OMP_NUM_THREADS=3 mpirun -npernode 12 ex4 $1
+		OMP_NUM_THREADS=3 mpirun -npernode 12 debug/ex4 $1
 	done
