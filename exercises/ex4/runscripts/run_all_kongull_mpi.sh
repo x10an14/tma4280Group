f18f9621329f887505a9a6348c2d896edfa7a88e
x10an14 <x10an14@hotmail.com>
2014-02-17 16:54:15 +0100

    Fixed typos/bugs...

--- exercises/ex4/runscripts/run_all_kongull_mpi.sh
+++ exercises/ex4/runscripts/run_all_kongull_mpi.sh
@@ -7,7 +7,6 @@ rm -rf ../release/*
 
 cd ../release/
 CXX=icpc CC=icc FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release  -DENABLE_OPENMP=0
-
 make clean && make
 chmod +x ex4
 
@@ -21,12 +20,26 @@ chmod +x 2n2ppn.sh
 chmod +x 2n4ppn.sh
 chmod +x 2n8ppn.sh
 
-cd kongull_outpt
+mkdir 1n1ppn -p && cd 1n1ppn
 qsub ../1n1ppn.sh
+
+mkdir 1n2ppn -p && cd ../1n2ppn/
 qsub ../1n2ppn.sh
+
+mkdir 1n4ppn -p && cd ../1n4ppn/
 qsub ../1n4ppn.sh
+
+mkdir 1n8ppn -p && cd ../1n8ppn/
 qsub ../1n8ppn.sh
+
+mkdir 2n1ppn -p && cd ../2n1ppn/
 qsub ../2n1ppn.sh
+
+mkdir 2n2ppn -p && cd ../2n2ppn/
 qsub ../2n2ppn.sh
+
+mkdir 2n4ppn -p && cd ../2n4ppn/
 qsub ../2n4ppn.sh
-qusb ../2n8ppn.sh
+
+mkdir 2n8ppn -p && cd ../2n8ppn/
+qsub ../2n8ppn.sh
