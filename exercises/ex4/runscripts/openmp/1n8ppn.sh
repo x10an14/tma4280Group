9c340934bb2a032c63558360e112ea64346477ed
x10an14 <x10an14@hotmail.com>
2014-02-17 16:05:01 +0100

    Added all OpenMP runfiles for kongull batch scheduling.

---
+++ exercises/ex4/runscripts/openmp/1n8ppn.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#PBS -N openmp_1n8ppn
+#PBS -A freecycle
+#PBS -q optimist
+#PBS -l walltime=00:08:00
+#PBS -l nodes=1:ppn=8:default
+#PBS -l pmem=1500MB
+#PBS -j oe
+
+cd ${PBS_O_WORKDIR}
+
+module load intelcomp
+module load openmpi/1.4.3-intel
+KMP_AFFINITY="granularity=fine, compact"
+
+for i in $(seq 3 28);
+	do	#There will be an empty newline between each For-loop iteration (AKA "\n\n")
+		echo 'k: '$i
+		OMP_NUM_THREADS=8 ../../../release/ex4 $i
+	done
---
+++ exercises/ex4/runscripts/run_all_kongull_openmp.sh
@@ -0,0 +1,31 @@
+cd /home/$USER/tma4280Group/exercises/ex4
+#Create CMAKE sub-folder
+mkdir release -p
+
+#Delete whatever was there before
+rm -rf release/*
+
+cd release/
+CXX=icpc CC=icc FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
+make clean && make
+
+chmod +x ex4
+
+cd ../runscripts/openmp/
+mkdir 1n1ppn 1n2ppn 1n4ppn 1n8ppn -p
+
+cd 1n1ppn/
+chmod +x ../n1ppn.sh
+qsub ../n1ppn.sh
+
+cd ../1n2ppn/
+chmod +x ../n2ppn.sh
+qsub ../n2ppn.sh
+
+cd ../1n4ppn/
+chmod +x ../n4ppn.sh
+qsub ../n4ppn.sh
+
+cd ../1n8ppn/
+chmod +x ../n8ppn.sh
+qsub ../n8ppn.sh
