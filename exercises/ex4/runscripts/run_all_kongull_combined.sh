d7080e036c841e8f56caf99d8703268817b03571
x10an14 <x10an14@hotmail.com>
2014-02-17 16:22:39 +0100

    Temp commit.

--- exercises/ex4/runscripts/run_all_kongull_combined.sh
+++ exercises/ex4/runscripts/run_all_kongull_combined.sh
@@ -1,5 +1,6 @@
-#Create CMAKE sub-folders
-mkdir debug release kongull_outpt -p
+cd /home/$USER/tma4280Group/exercises/ex4
+#Create CMAKE sub-folder
+mkdir release -p
 
 #Delete whatever was there before
 rm -rf release/*
@@ -8,19 +9,23 @@ cd release/
 CXX=icpc CC=icc FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 make clean && make
 
-cd ..
-chmod 755 release/ex4
-chmod 755 ex4_1n2ppnRel.sh
-chmod 755 ex4_1n4ppnRel.sh
-chmod 755 ex4_1n8ppnRel.sh
-chmod 755 ex4_2n1ppnRel.sh
-chmod 755 ex4_2n2ppnRel.sh
-chmod 755 ex4_2n4ppnRel.sh
+chmod +x ex4
 
-cd kongull_outpt
-qsub ../ex4_1n2ppnRel.sh
-qsub ../ex4_1n4ppnRel.sh
-qsub ../ex4_1n8ppnRel.sh
-qsub ../ex4_2n1ppnRel.sh
-qsub ../ex4_2n2ppnRel.sh
-qsub ../ex4_2n4ppnRel.sh
+cd ../runscripts/combined/
+mkdir 1n1ppn 1n2ppn 1n4ppn 1n8ppn -p
+
+cd 1n1ppn/
+chmod +x ../n1ppn.sh
+qsub ../n1ppn.sh
+
+cd ../1n2ppn/
+chmod +x ../n2ppn.sh
+qsub ../n2ppn.sh
+
+cd ../1n4ppn/
+chmod +x ../n4ppn.sh
+qsub ../n4ppn.sh
+
+cd ../1n8ppn/
+chmod +x ../n8ppn.sh
+qsub ../n8ppn.sh
