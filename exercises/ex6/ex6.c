a8c571394499cbdf9e4fef1a13852b3cfb485e78
x10an14 <x10an14@hotmail.com>
2014-03-03 14:13:35 +0100

    Renamed some copied files, and just barely started a little on ex6.c and .h.

---
+++ exercises/ex6/ex6.c
@@ -0,0 +1,7 @@
+#include "ex6.h"
+
+int int main(int argc, char *argv[]){
+	/* code */
+
+	return 0;
+}
---
+++ exercises/ex6/ex6.h
@@ -0,0 +1,19 @@
+#ifndef _EXERCISE_6_
+#define _EXERCISE_6_
+
+//All the existing .h files I could think of with potential use.
+#include "../../examples/common/common.h"
+#include "../../examples/common/blaslapack.h"
+#include "../../examples/poisson/solvers.h"
+#include "../../examples/poisson/possioncommon.h"
+
+#ifndef HAVE_MKL
+#define fst fst_
+#define fstinv fstinv_
+#endif
+
+/*Whatever we want specific to our solution/implementation.*/
+
+
+
+#endif
