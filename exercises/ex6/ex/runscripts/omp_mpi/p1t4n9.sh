766fe106129ab582d3a242cc4961d97e580b3d54
Erlend Sigholt <erlendsi@stud.ntnu.no>
2014-04-10 18:41:32 +0200

    some more to be run on kongull

---
+++ exercises/ex6/ex/runscripts/omp_mpi/p1t4n9.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#PBS -N combined_p6t2n1
+#PBS -A freecycle
+#PBS -q optimist
+#PBS -l walltime=00:10:00
+#PBS -l nodes=9:ppn=12:default
+###PBS -l pmem=750MB
+#PBS -j oe
+
+cd ${PBS_O_WORKDIR}
+
+module load intelcomp
+module load openmpi/1.4.3-intel
+KMP_AFFINITY="granularity=fine, compact"
+
+for i in $(seq 3 14);
+	do	#There will be an empty newline between each For-loop iteration (AKA "\n\n")
+		n=$(echo "2^$i" | bc)
+		echo 'n: '$n
+		OMP_NUM_THREADS=4 mpirun -npernode 1 ../../../release_omp_mpi/ex6 $n
+	done
